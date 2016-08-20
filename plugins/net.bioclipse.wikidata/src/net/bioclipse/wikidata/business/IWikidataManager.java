2ec4bb8d8da130fc334e1b61266ab549f3df30ca
Egon Willighagen <egon.willighagen@gmail.com>
2016-08-20 09:14:45 +0200

    Added methods around molecules in Wikidata

--- plugins/net.bioclipse.wikidata/src/net/bioclipse/wikidata/business/IWikidataManager.java
+++ plugins/net.bioclipse.wikidata/src/net/bioclipse/wikidata/business/IWikidataManager.java
@@ -11,11 +11,37 @@
 package net.bioclipse.wikidata.business;
 
 import net.bioclipse.core.PublishedClass;
+import net.bioclipse.core.PublishedMethod;
+import net.bioclipse.core.Recorded;
+import net.bioclipse.core.business.BioclipseException;
+import net.bioclipse.core.domain.IMolecule;
+import net.bioclipse.inchi.InChI;
 import net.bioclipse.managers.business.IBioclipseManager;
 
 @PublishedClass(
-    value="TODO: Describe the manager here."
+    value="Manager for interacting with the Wikidata database (mostly via the SPARQL end point)."
 )
 public interface IWikidataManager extends IBioclipseManager {
 
+    @Recorded
+    @PublishedMethod(
+        methodSummary="Return true if Wikidata contains a molecule with the given InChI.",
+        params="InChI inchi"
+    )
+    public boolean hasMolecule(InChI inchi) throws BioclipseException;
+
+    @Recorded
+    @PublishedMethod(
+        methodSummary="Return true the Wikidata entity ID for the molecule with the given InChI.",
+        params="InChI inchi"
+    )
+    public String getEntityID(InChI inchi) throws BioclipseException;
+
+    @Recorded
+    @PublishedMethod(
+        methodSummary="Return a molecule with the given InChI, or throws an BioclipseException"
+        		+ " when it does not exist.",
+        params="InChI inchi"
+    )
+    public IMolecule getMolecule(InChI inchi) throws BioclipseException;
 }
--- plugins/net.bioclipse.wikidata/src/net/bioclipse/wikidata/business/WikidataManager.java
+++ plugins/net.bioclipse.wikidata/src/net/bioclipse/wikidata/business/WikidataManager.java
@@ -10,14 +10,22 @@
  ******************************************************************************/
 package net.bioclipse.wikidata.business;
 
-import net.bioclipse.managers.business.IBioclipseManager;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 
-import org.apache.log4j.Logger;
+import net.bioclipse.core.business.BioclipseException;
+import net.bioclipse.core.domain.IMolecule;
+import net.bioclipse.core.domain.IStringMatrix;
+import net.bioclipse.inchi.InChI;
+import net.bioclipse.managers.business.IBioclipseManager;
+import net.bioclipse.rdf.Activator;
+import net.bioclipse.rdf.business.IRDFManager;
+import net.bioclipse.wikidata.domain.WikidataMolecule;
 
 public class WikidataManager implements IBioclipseManager {
 
-    private static final Logger logger = Logger.getLogger(WikidataManager.class);
-
+    private final IRDFManager rdf = Activator.getDefault().getJavaManager();
+    
     /**
      * Gives a short one word name of the manager used as variable name when
      * scripting.
@@ -25,4 +33,46 @@ public class WikidataManager implements IBioclipseManager {
     public String getManagerName() {
         return "wikidata";
     }
+
+    
+    public boolean hasMolecule(InChI inchi, IProgressMonitor monitor) throws BioclipseException {
+    	if (inchi == null) throw new BioclipseException("You must give an InChI.");
+    	if (monitor == null) monitor = new NullProgressMonitor();
+    	String hasMoleculeByInChI =
+    		"PREFIX wdt: <http://www.wikidata.org/prop/direct/>"
+    	   	+ "SELECT ?compound WHERE {"
+    	    + "  ?compound wdt:P235 \"" + inchi.getKey() + "\" ."
+    	    + "}";
+    	IStringMatrix results = rdf.sparqlRemote(
+    		"https://query.wikidata.org/sparql", hasMoleculeByInChI
+    	);
+    	return (results.getRowCount() > 0);
+    };
+
+    public String getEntityID(InChI inchi, IProgressMonitor monitor) throws BioclipseException {
+    	if (inchi == null) throw new BioclipseException("You must give an InChI.");
+    	if (monitor == null) monitor = new NullProgressMonitor();
+    	String hasMoleculeByInChI =
+        	"PREFIX wdt: <http://www.wikidata.org/prop/direct/>"
+        	+ "SELECT ?compound WHERE {"
+        	+ "  ?compound wdt:P235  \"" + inchi.getKey() + "\" ."
+        	+ "}";
+    	IStringMatrix results = rdf.sparqlRemote(
+    		"https://query.wikidata.org/sparql", hasMoleculeByInChI
+        );
+    	if (results.getRowCount() == 0)
+    		throw new BioclipseException("No molecule in Wikidata with the InChI: " + inchi);
+    	if (results.getRowCount() > 1)
+    		throw new BioclipseException("Too many molecules in Wikidata with the InChI: " + inchi);
+    	String entityID = results.get(1, "compound");
+    	if (entityID == null || entityID.length() == 0)
+    		throw new BioclipseException("No Wikidata entity found for the molecule with the InChI: " + inchi);
+    	return entityID;
+    }
+
+    public IMolecule getMolecule(InChI inchi, IProgressMonitor monitor) throws BioclipseException {
+    	if (inchi == null) throw new BioclipseException("You must give an InChI.");
+    	if (monitor == null) monitor = new NullProgressMonitor();
+    	return new WikidataMolecule(getEntityID(inchi, monitor));
+    };
 }
